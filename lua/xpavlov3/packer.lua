0b3c0d30513ac1adcb8e834b96ad7dcf3f900036
Filip Pavlovic <filpavlovic@gmail.com>
2023-06-12 23:45:35 +0200

    1st iteration

---
+++ lua/xpavlov3/packer.lua
@@ -0,0 +1,79 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim'
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use { "ellisonleao/gruvbox.nvim" }
+    require("gruvbox").setup({
+        undercurl = true,
+        underline = true,
+        bold = true,
+        italic = {
+            strings = false,
+            comments = true,
+            operators = false,
+            folds = true,
+        },
+        strikethrough = true,
+        invert_selection = false,
+        inver_sign = false,
+        invert_tabline = false,
+        inver_indent_guides = false,
+        inverse = false,
+        contrast = "",
+        paletter_overrides = {},
+        overrides = {},
+        dim_inactive = false,
+        transparent_mode = false,
+    })
+    vim.cmd("colorscheme gruvbox")
+    use('nvim-treesitter/nvim-treesitter', {run = 'TSUpdate'})
+    use('nvim-treesitter/playground')
+    use('theprimeagen/harpoon')
+    use('mbbill/undotree')
+    use('simrat39/rust-tools.nvim')
+    use('hrsh7th/cmp-nvim-lsp-signature-help')
+    use('hrsh7th/cmp-vsnip')
+    use('hrsh7th/cmp-path')
+    use('hrsh7th/cmp-buffer')
+    use('hrsh7th/vim-vsnip')
+    use('tpope/vim-fugitive')
+    use("arnamak/stay-centered.nvim")
+    use({ "Pocco81/auto-save.nvim",
+    config = function()
+        require("auto-save").setup {
+            execution_message = {
+                message = function()
+                    return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
+                end,
+                dim = 0.18, 
+                cleaning_interval = 10000, 
+            },
+        }
+    end,
+})
+use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    requires = {
+        {'neovim/nvim-lspconfig'},
+        {
+        'williamboman/mason.nvim',
+        run = function()
+            pcall(vim.cmd, 'MasonUpdate')
+        end,
+    },
+    {'williamboman/mason-lspconfig.nvim'},
+
+    {'hrsh7th/nvim-cmp'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'L3MON4D3/LuaSnip'},
+}
+
+}
+end)
+
+
