2048c6fafb26096ee40a3189d72f566fcf475cf3
Filip Pavlovic <filpavlovic@gmail.com>
2024-03-15 13:16:59 +0100

    chore: packer.lua refactor, compiled packer removed from repo

--- nvim/lua/wiesel/packer.lua
+++ nvim/lua/wiesel/packer.lua
@@ -11,6 +11,7 @@ return require('packer').startup(function(use)
     use { 'AlexvZyl/nordic.nvim' }
     use { "ellisonleao/gruvbox.nvim" }
     require("gruvbox").setup({
+
         undercurl = true,
         underline = true,
         bold = true,
@@ -33,55 +34,54 @@ return require('packer').startup(function(use)
         transparent_mode = true,
     })
     use ({
-	  'rose-pine/neovim',
-	  as = 'rose-pine',
-	  config = function()
-		  vim.cmd('colorscheme rose-pine')
-	end})
-    use{'jacoborus/tender.vim', as = 'tender' }
-    use('github/copilot.vim')
-    use('nvim-treesitter/nvim-treesitter', {run = 'TSUpdate'})
-    use('nvim-treesitter/playground')
-    use('theprimeagen/harpoon')
-    use('mbbill/undotree')
-    use('simrat39/rust-tools.nvim')
-    use('hrsh7th/cmp-nvim-lsp-signature-help')
-    use('hrsh7th/cmp-vsnip')
-    use('hrsh7th/cmp-path')
-    use('hrsh7th/cmp-buffer')
-    use('hrsh7th/vim-vsnip')
-    use('tpope/vim-fugitive')
-    use("arnamak/stay-centered.nvim")
-    use({ "Pocco81/auto-save.nvim",
-    config = function()
-        require("auto-save").setup {
-            execution_message = {
-                message = function()
-                    return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
-                end,
-                dim = 0.18,
-                cleaning_interval = 10000,
-            },
-        }
-    end,
-})
-use {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v2.x',
-    requires = {
-        {'neovim/nvim-lspconfig'},
-        {
-        'williamboman/mason.nvim',
-        run = function()
-            pcall(vim.cmd, 'MasonUpdate')
+        'rose-pine/neovim',
+        as = 'rose-pine',
+        config = function()
+            vim.cmd('colorscheme rose-pine')
+        end})
+        use{'jacoborus/tender.vim', as = 'tender' }
+        use('github/copilot.vim')
+        use('nvim-treesitter/nvim-treesitter', {run = 'TSUpdate'})
+        use('nvim-treesitter/playground')
+        use('theprimeagen/harpoon')
+        use('mbbill/undotree')
+        use('simrat39/rust-tools.nvim')
+        use('hrsh7th/cmp-nvim-lsp-signature-help')
+        use('hrsh7th/cmp-vsnip')
+        use('hrsh7th/cmp-path')
+        use('hrsh7th/cmp-buffer')
+        use('hrsh7th/vim-vsnip')
+        use('tpope/vim-fugitive')
+        use("arnamak/stay-centered.nvim")
+        use({ "Pocco81/auto-save.nvim",
+        config = function()
+            require("auto-save").setup {
+                execution_message = {
+                    message = function()
+                        return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
+                    end,
+                    dim = 0.18,
+                    cleaning_interval = 10000,
+                },
+            }
         end,
-    },
-    {'williamboman/mason-lspconfig.nvim'},
+    })
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            {'neovim/nvim-lspconfig'},
+            {
+                'williamboman/mason.nvim',
+                run = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            {'williamboman/mason-lspconfig.nvim'},
 
-    {'hrsh7th/nvim-cmp'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'L3MON4D3/LuaSnip'},
-}
-
-}
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'L3MON4D3/LuaSnip'},
+        }
+    }
 end)
