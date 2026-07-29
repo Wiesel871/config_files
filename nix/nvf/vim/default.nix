1acb0f468ed23865741a31b4d4fbb7c0178e9714
Filip Pavlovič <xpavlov3@fi.muni.cz>
2026-07-29 20:55:45 +0000

    feat: switch to dark grubox as vim theme

--- nix/nvf/vim/default.nix
+++ nix/nvf/vim/default.nix
@@ -1,4 +1,5 @@
-{pkgs, ...} @ inputs: {
+{ pkgs, ... }@inputs:
+{
   statusline.lualine.enable = true;
   telescope.enable = true;
   comments.comment-nvim.enable = true;
@@ -8,32 +9,32 @@
   lsp = import ./lsp.nix inputs;
   autocomplete = import ./autocomplete.nix inputs;
 
-  formatter = {};
+  formatter = { };
 
   theme = {
     enable = true;
-    name = "rose-pine";
-    style = "main";
+    /*
+      name = "rose-pine";
+      style = "main";
+    */
+    name = "gruvbox";
+    style = "dark";
   };
 
   startPlugins = with pkgs.vimPlugins; [
     harpoon
     undotree
-    vim-glsl
-    nvim-treesitter-parsers.glsl
-    nvim-lspconfig
+    #vim-glsl
+    #nvim-treesitter-parsers.glsl
+    #nvim-lspconfig
   ];
 
-  additionalRuntimePaths = ["$HOME/config_files/nvim"];
+  additionalRuntimePaths = [ "$HOME/config_files/nvim" ];
   luaConfigRC = {
-    myconfig =
-      /*
-      lua
-      */
-      ''
-        require("wiesel")
-        require("plugin")
-        vim.lsp.enable('glsl_analyzer')
-      '';
+    myconfig = /* lua */ ''
+      require("wiesel")
+      require("plugin")
+    '';
+    #vim.lsp.enable('glsl_analyzer')
   };
 }
